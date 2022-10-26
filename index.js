f63505cce95ada8b1d44c35a25420ab4a02fee7e
RN1216 <reunnoor@gmail.com>
2022-10-27 01:46:04 +0600

    Add CoursesDetail

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 
 app.use(cors());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const coursesdetail = require('./data/coursesdetail.json');
 
 app.get('/',(req,res)=>{
     res.send('Study Running');
@@ -13,6 +14,24 @@ app.get('/',(req,res)=>{
 
 app.get('/brisk-courses',(req,res)=>{
     res.send(courses)
+});
+
+app.get ('/category/:id',(req,res) =>{
+    const id = req.params.id;
+if(id === '07'){
+    res.send(coursesdetail);
+}
+else{
+    const category_coursesdetail = coursesdetail.filter(courseDetail => courseDetail.category_id === id);
+    res.send(category_coursesdetail);
+}
+});
+
+
+app.get ('/coursesdetail/:id', (req , res)=>{
+    const id =  req.params.id;
+    const selectedCoursesDetail = coursesdetail.find(courseDetail=>courseDetail._id === id);
+    res.send(selectedCoursesDetail);
 })
 
 app.listen(port, ()=>{
